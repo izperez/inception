cd70b7a35b64dd5fd074db23afa66e43bb4ff239
izperez <izperez@student.42urduliz.com>
2025-10-21 11:41:30 +0200

    funciona ok

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -6,4 +6,4 @@ chmod +x wp-cli.phar
 ./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
 ./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 
-php-fpm8.4 -F
+php-fpm8.2 -F
